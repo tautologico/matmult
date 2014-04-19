7741d50718fa372b661323a0f745512cf1f886b8
Andrei Formiga <archimedes_siracusa@hotmail.com>
2014-04-18 21:38:46 -0300

    Versions with loop unrolling

--- ocaml/matmult.ml
+++ ocaml/matmult.ml
@@ -39,6 +39,46 @@ let mat_mult m1 m2 =
     done;
     res
 
+(* Unroll inner loop at 2 iterations -- Only works for even sizes *)
+let mat_mult_2 m1 m2 = 
+  if m1.n <> m2.m then
+    failwith "Incompatible matrix dimensions"
+  else
+    let res = zero m1.m m2.n in
+    for i = 0 to res.m - 1 do
+      for j = 0 to res.n - 1 do
+        for p = 0 to (m1.n/2) - 1 do
+          let p1 = 2 * p in
+          let p2 = p1 + 1 in
+          set res i j ((get res i j) +. (get m1 i p1) *. (get m2 p1 j));
+          set res i j ((get res i j) +. (get m1 i p2) *. (get m2 p2 j))
+        done
+      done
+    done;
+    res
+
+(* Unroll inner loop at 4 iterations -- Only works for sizes multiple of 4 *)
+let mat_mult_4 m1 m2 = 
+  if m1.n <> m2.m then
+    failwith "Incompatible matrix dimensions"
+  else
+    let res = zero m1.m m2.n in
+    for i = 0 to res.m - 1 do
+      for j = 0 to res.n - 1 do
+        for p = 0 to (m1.n/4) - 1 do
+          let p1 = 4 * p in
+          let p2 = p1 + 1 in
+          let p3 = p2 + 1 in
+          let p4 = p3 + 1 in
+          set res i j ((get res i j) +. (get m1 i p1) *. (get m2 p1 j));
+          set res i j ((get res i j) +. (get m1 i p2) *. (get m2 p2 j));
+          set res i j ((get res i j) +. (get m1 i p3) *. (get m2 p3 j));
+          set res i j ((get res i j) +. (get m1 i p4) *. (get m2 p4 j))
+        done
+      done
+    done;
+    res
+
 let mat_mult_flops m1 m2 = 2.0 *. (float m1.m) *. (float m1.n) *. (float m2.n)
 
 let b_mat_mult_flops m1 m2 = 2.0 *. (float (dim1 m1)) *. (float (dim2 m1)) *. (float (dim2 m2))
@@ -73,11 +113,11 @@ let gen_b_mat_1 m n start inc =
   done;
   res
 
-let bench_mat_mult size =
+let bench_mat_mult mf size =
   let m1 = gen_mat_1 size size 0.0 0.01 in
   let m2 = gen_mat_1 size size 3.2 0.02 in
   let start_time = Unix.gettimeofday () in
-  let m3 = mat_mult m1 m2 in
+  let m3 = mf m1 m2 in
   let end_time = Unix.gettimeofday () in
   let elapsed = end_time -. start_time in
   let flops_sec = (mat_mult_flops m1 m2) /. (elapsed *. 1e9) in
@@ -114,12 +154,15 @@ let () =
   let low = get_arg_opt 1 200 in
   let inc = get_arg_opt 2 40 in
   let hi  = get_arg_opt 3 1200 in
-  Printf.printf "*** Array gemm\n";
+  Printf.printf "*** Standard (array) gemm\n";
   Printf.printf "N \t time \t GFLOPS/s\n";
-  bench_sizes low inc hi bench_mat_mult;
+  bench_sizes low inc hi (bench_mat_mult mat_mult);
 
-(*
-  Printf.printf "\n*** Bigarray gemm\n";
+  Printf.printf "\n*** Unroll 2 gemm\n";
   Printf.printf "N \t time \t GFLOPS/s\n";
-  bench_sizes low inc hi bench_b_mat_mult
-*)
+  bench_sizes low inc hi (bench_mat_mult mat_mult_2);
+
+  Printf.printf "\n*** Unroll 4 gemm\n";
+  Printf.printf "N \t time \t GFLOPS/s\n";
+  bench_sizes low inc hi (bench_mat_mult mat_mult_4)
+
